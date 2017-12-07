ca06b418cb6e6c737bec0e5a182648613a4f7ad1
James Rubingh <james@wrive.com>
2017-12-07 14:59:31 +0900

    create floppy image from source
    Signed-off-by: James Rubingh <james@wrive.com>

---
+++ create_floppy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+  echo "supply .asm file"
+  exit
+fi
+
+nasm $1 -f bin -o boot_sec.bin
+
+#show binary
+od -t x1 -A n boot_sec.bin
+
+dd if=/dev/zero of=floppy.img ibs=1k count=1440
+dd if=boot_sec.bin of=floppy.img conv=notrunc
+
+echo ""
+ls -l floppy.img
